ebf82f5adb8b803da6ce8969f9fa605122491a97
yusuke <ID+username@users.noreply.github.com>
2021-12-18 22:27:20 -0800

    fix post request

--- client/src/hooks/postRequest.ts
+++ client/src/hooks/postRequest.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import IAccount from '../interfaces/IAccount';
 import IPrivateInfo from '../interfaces/IPrivateInfo';
+
 const postRequest = async (endPoint: string, accountData: IAccount | IPrivateInfo, accessToken?: string | undefined | null) => {
   if (accessToken) {
     const headers = {
       "Content-type": "application/json",
+      'Access-Control-Allow-Origin':`${endPoint}}`,
       Authorization: `Bearer ${accessToken}`
     };
     return await axios.post(endPoint,
